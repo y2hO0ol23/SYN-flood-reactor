ca91f900109e4d5c389c2645070ce629bdf771f5
unknown <y2h000123>
2022-12-05 02:37:22 +0900

    a

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ def main():
     protect.syn.init()
     atexit.register(protect.syn.stop)
 
-    threads = (
+    threads = tuple(
         threading.Thread(target = sniff, kwargs={"prn" : protect.syn.handler, "count" : 0, "filter" : protect.syn.filter}, daemon=True)
     )
 
