f1ea9fa11bede20f1cc6e35931a9a5e5d90adeb0
unknown <y2h000123>
2022-12-05 03:23:36 +0900

    a

--- protect/syn.py
+++ protect/syn.py
@@ -12,19 +12,20 @@ def handler(packet: Packet):
 
     ip, seq, sport, dport = packet[IP].src, packet[TCP].seq, packet[TCP].sport, packet[TCP].dport
     key = "%s:%d seq=%d"%(ip, sport, seq)
-
-    if key not in check or not check[key]:
+    
+    if key not in check: check[key] = 0
+    if not check[key]:
         cmd = "-s %s -d %s --protocol tcp --sport %d --dport %d --tcp-flags SYN,ACK,FIN,RST SYN -j ACCEPT"%(ip, imports.ip, sport, dport)
 
-        key[check] = True
+        key[check] = 1
         os.system("iptables -I INPUT 1 %s"%cmd)
         
-        sniff(count=1, timeout=imports.timeout,
-            filter='src host %s and dst host %s and tcp[tcpflags] & (tcp-syn|tcp-ack|tcp-push) == tcp-syn and '%(ip, imports.ip) + \
-                   'tcp.sport == %d and tcp.dport == %d and tcp.seq == %d'%(sport, dport, seq))
+        while key[check] != 2: pass
 
         os.system("iptables -D INPUT %s"%cmd)
-        key[check] = False
+        key[check] = 0
+    else:
+        check[key] = 2
 
 
 def init()->None:
