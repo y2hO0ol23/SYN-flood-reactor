b8b9712f009dbf8a46f375b6586616501a30a618
unknown <y2h000123>
2022-12-05 03:13:47 +0900

    a

--- protect/syn.py
+++ protect/syn.py
@@ -5,21 +5,29 @@ import threading
 
 from scapy.layers.inet import IP, TCP
 
-global end
+global end, check
 
 def handler(packet: Packet):
+    global check
+
     ip, seq, sport, dport = packet[IP].src, packet[TCP].seq, packet[TCP].sport, packet[TCP].dport
-    cmd = "-s %s -d %s --protocol tcp --sport %d --dport %d --tcp-flags SYN,ACK,FIN,RST SYN -j ACCEPT"%(ip, imports.ip, sport, dport)
+    key = "%s:%d seq=%d"%(ip, sport, seq)
 
-    os.system("iptables -I INPUT 1 %s"%cmd)
-    
-    sniff(count=1, filter='src host %s and dst host %s and tcp[tcpflags] & (tcp-syn|tcp-ack|tcp-push) == tcp-syn and tcp.sport == %d and tcp.dport == %d and tcp.seq == %d'%(ip, imports.ip, sport, dport, seq))
+    if key not in check or not check[key]:
+        cmd = "-s %s -d %s --protocol tcp --sport %d --dport %d --tcp-flags SYN,ACK,FIN,RST SYN -j ACCEPT"%(ip, imports.ip, sport, dport)
 
-    os.system("iptables -D INPUT %s"%cmd)
+        key[check] = True
+        os.system("iptables -I INPUT 1 %s"%cmd)
+        
+        sniff(count=1, filter='src host %s and dst host %s and tcp[tcpflags] & (tcp-syn|tcp-ack|tcp-push) == tcp-syn and tcp.sport == %d and tcp.dport == %d and tcp.seq == %d'%(ip, imports.ip, sport, dport, seq))
+
+        os.system("iptables -D INPUT %s"%cmd)
+        key[check] = False
 
 
 def init()->None:
-    global end
+    global end, check
+    check = dict()
     end = False
 
 
