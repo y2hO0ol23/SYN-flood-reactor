1c9d29e060275d580e999658f633ed43ec82c239
unknown <y2h000123>
2022-12-05 01:22:53 +0900

    a

--- clear_iptables.py
+++ clear_iptables.py
@@ -2,4 +2,4 @@ import imports
 import os
 
 drop_all_syn = '-d %s --protocol tcp --tcp-flags SYN,RST,ACK,FIN SYN -j DROP'%imports.ip
-os.system('iptables -I INPUT 1 %s'%drop_all_syn)
+os.system('iptables -D INPUT %s'%drop_all_syn)
