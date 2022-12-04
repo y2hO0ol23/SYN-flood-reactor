de65d4f01f5e326b521892ed3d08db9e3b0017bb
unknown <y2h000123>
2022-12-05 01:05:25 +0900

    a

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import protect.core
 import imports
 import os
 
-drop_all_syn = '-d %s --protocol tcp --tcp-flags SYN,RST,ACK,FIN SYN -j DROP'%imports.IP6Field
+drop_all_syn = '-d %s --protocol tcp --tcp-flags SYN,RST,ACK,FIN SYN -j DROP'%imports.ip
 
 if __name__ == '__main__':  
     os.system('iptables -I INPUT 1 %s'%drop_all_syn)
